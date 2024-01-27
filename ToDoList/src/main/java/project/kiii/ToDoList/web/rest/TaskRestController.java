5fa6c364dbae0542d35099d94c6db1ce59714c7a
TeaK16 <93773068+TeaK16@users.noreply.github.com>
2024-01-27 14:05:26 +0100

    Update TaskRestController.java

--- ToDoList/src/main/java/project/kiii/ToDoList/web/rest/TaskRestController.java
+++ ToDoList/src/main/java/project/kiii/ToDoList/web/rest/TaskRestController.java
@@ -12,7 +12,7 @@ import project.kiii.ToDoList.services.impl.TaskServiceImpl;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://todo-service:3000")
 @RequestMapping("/api")
 public class TaskRestController {
 
