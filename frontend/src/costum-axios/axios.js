40ea3da14d0e2a6c3937f34bafd1a69db20b226b
TeaK16 <93773068+TeaK16@users.noreply.github.com>
2024-01-28 22:15:23 +0100

    Update axios.js

--- frontend/src/costum-axios/axios.js
+++ frontend/src/costum-axios/axios.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
+const baseURLs = {
+  backend: 'http://todo-backend-service.todo-namespace/api',
+  frontend: 'http://todo-app-service.todo-namespace/api',
+};
+
 const instance = axios.create({
-    baseURL: 'http://todo-backend-service.todo-namespace/api',
+  baseURL: baseURLs.backend,
+});
+
+// You can dynamically switch baseURL for specific requests
+instance.interceptors.request.use((config) => {
+  // Example: set baseURL to frontend for requests to /frontend/*
+  if (config.url.startsWith('/frontend/')) {
+    config.baseURL = baseURLs.frontend;
+  }
+
+  return config;
 });
 
 export default instance;
