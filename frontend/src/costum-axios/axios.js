7181f9ce7e16c23e2272b8523838b07c61f6e07b
TeaK16 <93773068+TeaK16@users.noreply.github.com>
2024-01-28 22:49:50 +0100

    Update axios.js

--- frontend/src/costum-axios/axios.js
+++ frontend/src/costum-axios/axios.js
@@ -4,4 +4,14 @@ const instance = axios.create({
     baseURL: 'http://todo-backend-service.todo-namespace/api', 
 });
 
+instance.interceptors.request.use((config) => {
+  // Modify the headers to allow cross-origin requests
+  config.headers['Access-Control-Allow-Origin'] = 'http://todo-app-service.todo-namespace';
+  config.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS, PUT, PATCH, DELETE';
+  config.headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, X-Auth-Token';
+
+  // Optionally, you can set additional headers or modify other request properties here
+
+  return config;
+});
 export default instance;
